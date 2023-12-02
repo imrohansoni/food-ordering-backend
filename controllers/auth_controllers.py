a92b419931294b64bb073d61c2a213ab2d49b91f
rohan soni <rohansoni54476@gmail.com>
2023-12-02 21:41:47 +0530

    add controllers and routers for updating mobile number

--- controllers/auth_controllers.py
+++ controllers/auth_controllers.py
@@ -24,6 +24,81 @@ def generate_hash(data):
 
 
 @exception_handler
+def verify_code(handler):
+    @wraps(handler)
+    def decorator_fun(*args, **kwargs):
+        data = request.get_json()
+        validator = (Validator(data)
+                     .field("mobile_number")
+                     .required("Please provide the mobile number")
+                     .field("code")
+                     .required("please provide the verification code")
+                     .field("expires_at")
+                     .required("Please provide the expires at")
+                     .field("hash")
+                     .required("Please provide the hash")
+                     .execute())
+
+        mobile_number = validator.get("mobile_number")
+        code = validator.get("code")
+        expires_at = validator.get("expires_at")
+        user_hash = validator.get("hash")
+
+        if datetime.now(UTC) > datetime.strptime(expires_at, "%Y-%m-%d %H:%M:%S.%f%z"):
+            return jsonify({
+                "status": "fail",
+                "message": "verification code is expired"
+            }), 400
+
+        hash_string = generate_hash({
+            "mobile_number": mobile_number,
+            "code": code,
+            "expires_at": expires_at,
+        })
+
+        if hash_string != user_hash:
+            return jsonify({
+                "status": "fail",
+                "message": "wrong verification code, please try again"
+            }), 400
+
+        return handler(*args, **kwargs)
+    return decorator_fun
+
+
+@exception_handler
+def login():
+    data = request.get_json()
+    user = db["users"].find_one({
+        "mobile_number": data.get("mobile_number")
+    })
+
+    user_id = None
+
+    if user is None:
+        result = db["users"].insert_one({
+            "mobile_number": data.get("mobile_number"),
+            "created_at": datetime.now(UTC),
+            "user_type": UserTypes.CUSTOMER.value,
+            "last_updated_at": datetime.now(UTC)
+        })
+        user_id = result.inserted_id
+    else:
+        user_id = user.get("_id")
+
+    auth_token = encode({
+        "user_id": str(user_id)
+    }, os.getenv("JWT_SECRET_KEY"))
+
+    return jsonify({
+        "status": "success",
+        "data": {
+            "auth_token": auth_token
+        }
+    })
+
+
+@exception_handler
 def send_code():
     validator = (Validator(request.get_json())
                  .field("mobile_number")
@@ -57,68 +132,55 @@ def send_code():
 
 
 @exception_handler
-def verify_code():
-    data = request.get_json()
-    validator = (Validator(data)
-                 .field("mobile_number")
-                 .required("Please provide the mobile number")
-                 .field("code")
-                 .required("please provide the verification code")
-                 .field("expires_at")
-                 .required("Please provide the expires at")
-                 .field("hash")
-                 .required("Please provide the hash")
-                 .execute())
+def check_mobile_number(handler):
+    @wraps(handler)
+    def decorator_fun(*args, **kwargs):
+        validator = (Validator(request.get_json())
+                     .field("mobile_number")
+                     .required("Please enter the mobile number")
+                     .execute())
 
-    mobile_number = validator.get("mobile_number")
-    code = validator.get("code")
-    expires_at = validator.get("expires_at")
-    user_hash = validator.get("hash")
+        mobile_number = validator.get("mobile_number")
 
-    if datetime.now(UTC) > datetime.strptime(expires_at, "%Y-%m-%d %H:%M:%S.%f%z"):
-        return jsonify({
-            "status": "fail",
-            "message": "verification code is expired"
-        }), 400
-
-    hash_string = generate_hash({
-        "mobile_number": mobile_number,
-        "code": code,
-        "expires_at": expires_at,
-    })
-
-    if hash_string != user_hash:
-        return jsonify({
-            "status": "fail",
-            "message": "wrong verification code, please try again"
-        }), 400
-
-    data = request.get_json()
-
-    user = db["users"].find_one({
-        "mobile_number": data.get("mobile_number")
-    })
-
-    user_id = None
-
-    if user is None:
-        result = db["users"].insert_one({
-            "mobile_number": data.get("mobile_number"),
-            "created_at": datetime.now(UTC),
-            "user_type": UserTypes.CUSTOMER.value,
-            "last_updated_at": datetime.now(UTC)
+        user = db.get_collection("users").find_one({
+            "mobile_number": mobile_number
+        }, {
+            "$projection": {
+                "mobile_number": 1
+            }
         })
-        user_id = result.inserted_id
-    else:
-        user_id = user.get("_id")
 
-    auth_token = encode({
-        "user_id": str(user_id)
-    }, os.getenv("JWT_SECRET_KEY"))
+        if user is not None:
+            return jsonify({
+                "status": "fail",
+                "message": "please use different number"
+            })
+
+        return handler(*args, **kwargs)
+    return decorator_fun
+
+
+@exception_handler
+def update_mobile():
+    mobile_number = request.get_json()
+    user_id = g.user_data.get("_id")
+
+    result = db.get_collection("users").update_one({
+        "_id": ObjectId(user_id),
+        "active": True
+    }, {
+        "$set": {
+            "mobile_number": mobile_number
+        }
+    })
+
+    if result.matched_count < 1:
+        return jsonify({
+            "status": "fail",
+            "message": "user not found"
+        }), 404
 
     return jsonify({
         "status": "success",
-        "data": {
-            "auth_token": auth_token
-        }
+        "message": "your number is updated"
     })
--- routers/auth_routers.py
+++ routers/auth_routers.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-from controllers.auth_controllers import send_code, verify_code
+from controllers.auth_controllers import send_code, verify_code, login, check_mobile_number, update_mobile
+from decorators.auth_decorators import authorize
 
 auth_blueprint = Blueprint("authentications", __name__)
 
@@ -9,6 +10,21 @@ def send_code_route():
     return send_code()
 
 
-@auth_blueprint.post("/verify-code")
-def verify_code_route():
-    return verify_code()
+@auth_blueprint.post("/login")
+@verify_code
+def login_route():
+    return login()
+
+
+@auth_blueprint.post("/update-mobile/send-code")
+@authorize
+@check_mobile_number
+def update_mobile_send_code_route():
+    return send_code()
+
+
+@auth_blueprint.post("/update-mobile")
+@authorize
+@verify_code
+def update_mobile_route():
+    return update_mobile()
