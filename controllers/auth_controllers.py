2fd905a52ed5c49f24ade40fc606d3414acad744
rohan soni <rohansoni54476@gmail.com>
2023-12-03 08:12:04 +0530

    move verify_code and check_mobile_number decorator function into auth_decorators

--- controllers/auth_controllers.py
+++ controllers/auth_controllers.py
@@ -24,49 +24,6 @@ def generate_hash(data):
 
 
 @exception_handler
-def verify_code(handler):
-    @wraps(handler)
-    def decorator_fun(*args, **kwargs):
-        data = request.get_json()
-        validator = (Validator(data)
-                     .field("mobile_number")
-                     .required("Please provide the mobile number")
-                     .field("code")
-                     .required("please provide the verification code")
-                     .field("expires_at")
-                     .required("Please provide the expires at")
-                     .field("hash")
-                     .required("Please provide the hash")
-                     .execute())
-
-        mobile_number = validator.get("mobile_number")
-        code = validator.get("code")
-        expires_at = validator.get("expires_at")
-        user_hash = validator.get("hash")
-
-        if datetime.now(UTC) > datetime.strptime(expires_at, "%Y-%m-%d %H:%M:%S.%f%z"):
-            return jsonify({
-                "status": "fail",
-                "message": "verification code is expired"
-            }), 400
-
-        hash_string = generate_hash({
-            "mobile_number": mobile_number,
-            "code": code,
-            "expires_at": expires_at,
-        })
-
-        if hash_string != user_hash:
-            return jsonify({
-                "status": "fail",
-                "message": "wrong verification code, please try again"
-            }), 400
-
-        return handler(*args, **kwargs)
-    return decorator_fun
-
-
-@exception_handler
 def login():
     data = request.get_json()
     user = db["users"].find_one({
@@ -129,35 +86,6 @@ def send_code():
             "expires_at": expires_at
         }
     })
-
-
-@exception_handler
-def check_mobile_number(handler):
-    @wraps(handler)
-    def decorator_fun(*args, **kwargs):
-        validator = (Validator(request.get_json())
-                     .field("mobile_number")
-                     .required("Please enter the mobile number")
-                     .execute())
-
-        mobile_number = validator.get("mobile_number")
-
-        user = db.get_collection("users").find_one({
-            "mobile_number": mobile_number
-        }, {
-            "$projection": {
-                "mobile_number": 1
-            }
-        })
-
-        if user is not None:
-            return jsonify({
-                "status": "fail",
-                "message": "please use different number"
-            })
-
-        return handler(*args, **kwargs)
-    return decorator_fun
 
 
 @exception_handler
--- decorators/auth_decorators.py
+++ decorators/auth_decorators.py
@@ -5,6 +5,8 @@ from database.db import db
 from bson import ObjectId
 import os
 from jsonwebtoken import decode, encode
+from utils.validator import Validator
+from datetime import datetime, UTC
 
 
 @exception_handler
@@ -64,3 +66,75 @@ def access_permission(authorized_users: list):
         return decorated_fun
 
     return decorated_wrapper
+
+
+@exception_handler
+def check_mobile_number(handler):
+    @wraps(handler)
+    def decorator_fun(*args, **kwargs):
+        validator = (Validator(request.get_json())
+                     .field("mobile_number")
+                     .required("Please enter the mobile number")
+                     .execute())
+
+        mobile_number = validator.get("mobile_number")
+
+        user = db.get_collection("users").find_one({
+            "mobile_number": mobile_number
+        }, {
+            "$projection": {
+                "mobile_number": 1
+            }
+        })
+
+        if user is not None:
+            return jsonify({
+                "status": "fail",
+                "message": "please use different number"
+            })
+
+        return handler(*args, **kwargs)
+    return decorator_fun
+
+
+@exception_handler
+def verify_code(handler):
+    @wraps(handler)
+    def decorator_fun(*args, **kwargs):
+        data = request.get_json()
+        validator = (Validator(data)
+                     .field("mobile_number")
+                     .required("Please provide the mobile number")
+                     .field("code")
+                     .required("please provide the verification code")
+                     .field("expires_at")
+                     .required("Please provide the expires at")
+                     .field("hash")
+                     .required("Please provide the hash")
+                     .execute())
+
+        mobile_number = validator.get("mobile_number")
+        code = validator.get("code")
+        expires_at = validator.get("expires_at")
+        user_hash = validator.get("hash")
+
+        if datetime.now(UTC) > datetime.strptime(expires_at, "%Y-%m-%d %H:%M:%S.%f%z"):
+            return jsonify({
+                "status": "fail",
+                "message": "verification code is expired"
+            }), 400
+
+        hash_string = generate_hash({
+            "mobile_number": mobile_number,
+            "code": code,
+            "expires_at": expires_at,
+        })
+
+        if hash_string != user_hash:
+            return jsonify({
+                "status": "fail",
+                "message": "wrong verification code, please try again"
+            }), 400
+
+        return handler(*args, **kwargs)
+    return decorator_fun
