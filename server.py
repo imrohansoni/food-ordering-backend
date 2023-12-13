fc94d86c661f030cebe93186a2fca97ee3593d16
rohan soni <rohansoni54476@gmail.com>
2023-12-13 18:54:07 +0530

    move the app logic into app file

--- server.py
+++ server.py
@@ -1,42 +1,14 @@
-from flask import Flask, request, jsonify
-from routers.auth_routers import auth_blueprint
-from routers.product_routers import product_blueprint
-from routers.category_routers import category_bp
-import traceback
-import config
-app = Flask(__name__)
+import os
 
+from app import app
 
-app.register_blueprint(product_blueprint, url_prefix="/api/v1/products")
-app.register_blueprint(auth_blueprint, url_prefix="/api/v1/auth")
-app.register_blueprint(category_bp, url_prefix="/api/v1/categories")
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return jsonify({
-        "status": "fail",
-        "message": f"{request.path} is not found"
-    }), 404
-
-
-@app.errorhandler(405)
-def method_not_found(error):
-    return jsonify({
-        "status": "fail",
-        "message": f"{request.method} is not defined on {request.path}"
-    }), 405
-
-
-@app.errorhandler(Exception)
-def handle_exception(error):
-    print(traceback.format_exc())
-    print(error)
-    return jsonify({
-        "status": "error",
-        "message": "something went wrong"
-    }), 500
-
+environment = os.environ.get("FLASK_ENV")
+if environment == "development":
+    print("server is running is development environment 🚀")
+elif environment == "production":
+    print("server is running in production environment 🚀")
+else:
+    print("server is running without any environment ⛔")
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host="localhost", port=5000, debug=True, load_dotenv=True)
