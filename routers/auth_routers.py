7d6f4173f6cc99723113641b22086a401eabd830
rohan soni <rohansoni54476@gmail.com>
2023-12-21 21:44:42 +0530

    add admin-login and change-password routes

--- routers/auth_routers.py
+++ routers/auth_routers.py
@@ -1,29 +1,48 @@
 from flask import Blueprint
-from controllers.auth_controllers import send_code, verify_code, login, check_mobile_number, update_mobile
-from decorators.auth_decorators import authorize
+from controllers.auth_controllers import forgot_password, login_admin, send_code, login, change_password
+from decorators.auth_decorators import access_permission, authorize, verify_code, check_mobile_number
+from controllers.account_controllers import update_mobile
+from utils.constants import UserTypes
 
-auth_blueprint = Blueprint("authentications", __name__)
+auth_bp = Blueprint("authentications", __name__)
 
 
-@auth_blueprint.post("/send-code")
+@auth_bp.post("/send-code")
 def send_code_route():
     return send_code()
 
 
-@auth_blueprint.post("/login")
+@auth_bp.post("/login")
 @verify_code
 def login_route():
     return login()
 
 
-@auth_blueprint.post("/update-mobile/send-code")
+@auth_bp.post("/admin/login")
+def login_admin_route():
+    return login_admin()
+
+
+@auth_bp.post("/admin/forgot-password")
+def forgot_password_route():
+    return forgot_password()
+
+
+@auth_bp.post("/admin/change-password")
+@authorize
+@access_permission(UserTypes.ADMIN.value)
+def change_password_route():
+    return change_password()
+
+
+@auth_bp.post("/update-mobile/send-code")
 @authorize
 @check_mobile_number
 def update_mobile_send_code_route():
     return send_code()
 
 
-@auth_blueprint.post("/update-mobile")
+@auth_bp.post("/update-mobile")
 @authorize
 @verify_code
 def update_mobile_route():
