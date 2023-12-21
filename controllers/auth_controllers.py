acd2f85e89b178aa369bf7a5251e854d2d200416
rohan soni <rohansoni54476@gmail.com>
2023-12-21 08:08:13 +0530

    add admin login & updated password function

--- controllers/auth_controllers.py
+++ controllers/auth_controllers.py
@@ -1,17 +1,19 @@
 import os
+from sys import exception
+from bson import ObjectId
 
 import requests
 
 from database.db import db
-from flask import request, jsonify, g, current_app
+from flask import request, jsonify, g
 from datetime import datetime, UTC, timedelta
 from random import randint
 from jsonwebtoken import encode
-from bson import ObjectId
 from utils.validator import Validator
 from utils.methods import exception_handler, generate_hash
 from utils.constants import UserTypes
 from utils.response_messages import *
+from bcrypt import checkpw, gensalt, hashpw
 
 
 @exception_handler
@@ -34,7 +36,7 @@ def login():
 
     auth_token = encode({
         "user_id": str(user_id)
-    }, current_app.config["JWT_SECRET_KEY"])
+    }, os.environ.get("JWT_SECRET_KEY"))
 
     return jsonify({
         "status": "success",
@@ -54,14 +56,12 @@ def send_code():
 
     mobile_number = validator.get("mobile_number")
 
-    # the verification code will be generated between 1234 and 9876
     code = randint(1234, 9876)
 
     if os.environ.get("FLASK_ENV") == "production":
-        # send the verification code through sms only in production environment
-        sms_response = requests.get(current_app.config["SMS_BASE_URL"], params={
+        sms_response = requests.get(os.environ.get("SMS_BASE_URL"), params={
             "route": "otp",
-            "authorization": current_app.config["SMS_AUTHORIZATION"],
+            "authorization": os.environ.get("SMS_AUTHORIZATION"),
             "variables_values": code,
             "flash": 0,
             "numbers": mobile_number
@@ -71,14 +71,12 @@ def send_code():
         if response.get("return") is False:
             print(response.get("message"))
     else:
-        # print the verification code in console in development environment
         print(f"verification code = {code}")
 
     current_time = datetime.now(UTC)
 
-    # after 3 minutes verification code will be expired
     expires_at = str(
-        current_time + timedelta(minutes=int(current_app.config["VERIFICATION_CODE_EXPIRES"])))
+        current_time + timedelta(minutes=int(os.environ.get("VERIFICATION_CODE_EXPIRES"))))
 
     hash_string = generate_hash({
         "mobile_number": mobile_number,
@@ -97,93 +95,84 @@ def send_code():
 
 
 @exception_handler
-def update_mobile():
-    data = request.get_json()
-    user_id = g.user_data.get("_id")
+def login_admin():
+    data = (Validator(request.get_json())
+            .field("mobile_number")
+            .required("please provide the mobile number")
+            .field("password")
+            .required("please provide the password")
+            .execute())
+
+    admin = db.get_collection("users").find_one({
+        "mobile_number": data.get("mobile_number"),
+        "user_type": "admin"
+    }, projection={
+        "mobile_number": 1,
+        "password_updated_at": 1,
+        "password": 1
+    })
+
+    if admin is None:
+        return jsonify({
+            "status": "fail",
+            "message": "account not found"
+        })
+
+    password_matched = checkpw(data.get("password"), admin["password"])
+
+    if not password_matched:
+        return jsonify({
+            "status": "fail",
+            "message": "password is incorrect, try again"
+        })
+
+    auth_token = encode({
+        "user_id": str(admin["_id"])
+    }, os.environ.get("JWT_SECRET_KEY"))
+
+    return jsonify({
+        "status": "success",
+        "data": {
+            "auth_token": auth_token
+        }
+    }), 200
+
+
+@exception_handler
+def change_password():
+    data = (Validator(request.get_json())
+            .field("old_password")
+            .required("please provide the old password")
+            .field("new_password")
+            .required("please provide the new password")
+            .execute())
+
+    current_user_id = g.user_data.get("_id")
+
+    password_matched = checkpw(
+        data.get("old_password"), g.user_data.get("password"))
+
+    if not password_matched:
+        return jsonify({
+            "status": "fail",
+            "message": "password is incorrect, try again"
+        })
 
     result = db.get_collection("users").update_one({
-        "_id": ObjectId(user_id),
+        "_id": ObjectId(current_user_id)
     }, {
         "$set": {
-            "mobile_number": data.get("mobile_number")
+            "password": hashpw(data.get("new_password"), gensalt(12))
         }
     })
-
-    if result.matched_count < 1:
-        return jsonify({
-            "status": "fail",
-            "message": "user not found"
-        }), 404
-
-    return jsonify({
-        "status": "success",
-        "message": "your number is updated"
-    })
-
-
-@exception_handler
-def update_name():
-    user_id = g.user_data.get("_id")
-    data = Validator(request.get_json()).field("first_name").required(
-        "please enter your name").field("last_name").required("please enter the last name").execute()
-
-    result = db["users"].update_one({
-        "_id": ObjectId(user_id)
-    }, {
-        "$set": {
-            "first_name": data.get("first_name"),
-            "last_name": data.get("last_name")
-        }
-    })
-
-    if result.matched_count < 1:
-        return jsonify({
-            "status": "fail",
-            "message": "user not found"
-        }), 404
 
     if result.modified_count < 1:
         return jsonify({
             "status": "fail",
-            "message": "failed to updated"
-        }), 400
+            "message": "failed to update the password"
+        })
 
     return jsonify({
         "status": "success",
-        "message": "your name is updated"
-    })
-
-
-@exception_handler
-def update_location():
-    user_id = g.user_data.get("_id")
-    data = Validator(request.get_json()).field("lat").required(
-        "please enter your name").field("lng").required("please enter the last name").execute()
-
-    result = db["users"].update_one({
-        "_id": ObjectId(user_id)
-    }, {
-        "$set": {
-            "location": {
-                "type": "Point",
-                "coordinates": [data.get("lat"), data.get("lng")]
-            }
-        }
-    })
-
-    if result.matched_count < 1:
-        return jsonify({
-            "status": "fail",
-            "message": "user not found"
-        }), 404
-
-    if result.modified_count < 1:
-        return jsonify({
-            "status": "fail",
-            "message": "failed to updated"
-        }), 400
-
-    return jsonify({
-        "status": "success",
-        "message": "location is updated"
+        "message": "password is updated"
     })
