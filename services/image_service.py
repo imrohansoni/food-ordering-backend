ebfa9dcb927708194794ddaefbd8cff89aa652fb
rohan soni <rohansoni54476@gmail.com>
2023-12-25 19:52:53 +0530

    refactor and fix bug

--- services/image_service.py
+++ services/image_service.py
@@ -1,9 +1,9 @@
-from werkzeug.utils import secure_filename
 import uuid
 import os
+
+from werkzeug.utils import secure_filename
 from cloudinary.utils import cloudinary_url
 from cloudinary.uploader import upload
-from bson import ObjectId
 
 
 def upload_images(files, foldername):
@@ -18,25 +18,27 @@ def upload_images(files, foldername):
             image_filename = f"{code}{secure_filename(file.filename)}"
             upload_path = os.path.join(f"uploads/{foldername}", image_filename)
             file.save(upload_path)
+
             uploaded_images.append({
-                "_id": ObjectId(),
                 "image_url": os.path.abspath(upload_path),
                 "image_id": image_filename
             })
     else:
-        upload_result = upload(file,
-                               public_id=f"{code}{
-                                   secure_filename(file.filename)}",
-                               overwrite=True,
-                               folder=foldername)
+        for file in files:
+            code = str(uuid.uuid4()).replace("-", "")
+            upload_result = upload(file,
+                                   public_id=f"{code}{
+                                       secure_filename(file.filename)}",
+                                   overwrite=True,
+                                   folder=foldername)
 
-        image_id = upload_result['public_id']
-        image_url, _ = cloudinary_url(image_id,
-                                      format=upload_result['format'])
-        uploaded_images.append({
-            "_id": str(ObjectId()),
-            "image_url": image_url,
-            "image_id": image_id
-        })
+            public_id = upload_result['public_id']
+            image_url, _ = cloudinary_url(public_id,
+                                          format=upload_result['format'])
+
+            uploaded_images.append({
+                "image_url": image_url,
+                "image_id": public_id
+            })
 
     return uploaded_images
